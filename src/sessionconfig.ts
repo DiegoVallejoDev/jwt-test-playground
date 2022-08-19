1bf9d1572b8255ac383f3415e9be8d88736d530e
Diego Vallejo <die.1702@hotmail.com>
2022-08-19 14:31:53 -0500

    update session time

--- src/sessionconfig.ts
+++ src/sessionconfig.ts
@@ -1,5 +1,5 @@
 export const config = Object.freeze({
     secret: "secret61789das8duauca9", // should be imported from .env file but this is a toy project
-    SESSION_TIMEOUT_MINUTES: 1,
+    SESSION_TIMEOUT_MINUTES: 2,
     REFRESH_TIMEOUT_MINUTES: 10,
 })
