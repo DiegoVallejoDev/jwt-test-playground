9562f6e5a65b3994b63014896c7ac3baa4351699
DiegoVallejoDev <die.1702@hotmail.com>
2022-08-19 12:31:25 -0500

    timer fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -322,7 +322,7 @@ const ExpirationCountdown = ({ exp }: { exp: number }) => {
 const Timer = ({ seconds }: { seconds: number }) => {
   // return seconds in minutes and seconds
   const minutes = Math.floor(seconds / 60);
-  const secondsLeft = seconds - minutes * 60;
+  const secondsLeft = seconds - (minutes * 60);
   return (
     <div>
       {minutes}:{secondsLeft < 10 ? "0" + secondsLeft : secondsLeft}
